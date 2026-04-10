00be59f7c541a3803816d1afe002652d1764b7fc
Syeda Aleema Abbas Zaidi <alimaabbaszaidi100@gmail.com>
2026-04-10 10:47:17 +0530

    Week_9

--- Week_9/Q1.py
+++ Week_9/Q1.py
@@ -9,14 +9,14 @@ data = text.encode()
 
 print("Original Text:", text)
 
-# 🔸 SHA-1
+#SHA-1
 sha1_hash = hashlib.sha1(data).hexdigest()
 print("\nSHA-1 :", sha1_hash)
 
-# 🔸 SHA-2 (using SHA-256)
+#SHA-2 (using SHA-256)
 sha2_hash = hashlib.sha256(data).hexdigest()
 print("SHA-2 (SHA-256):", sha2_hash)
 
-# 🔸 SHA-3 (using SHA3-256)
+#SHA-3 (using SHA3-256)
 sha3_hash = hashlib.sha3_256(data).hexdigest()
 print("SHA-3 (SHA3-256):", sha3_hash)
